fe0fabea47b773bed72320eaeb4050e4e41039aa
Jinlong Ru@hms <jinlong.ru@helmholtz-muenchen.de>
2021-01-30 11:16:18 +0100

    mapping prophages to reference using mummer

---
+++ bin/mapping_prophage_to_new_ref.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import click
+from Bio import SeqIO
+from pymummer import coords_file, alignment, nucmer
+
+
+def run_nucmer(qry, ref):
+    with open('tmp_qry.fna', 'w') as fh:
+        SeqIO.write(qry, fh, "fasta")
+    with open('tmp_ref.fna', 'w') as fh:
+        SeqIO.write(ref, fh, "fasta")
+
+    runner = nucmer.Runner("tmp_ref.fna", "tmp_qry.fna", "tmp_mm.out", maxgap=200, mincluster=1000) 
+    runner.run()
+    file_reader = coords_file.reader("tmp_mm.out")
+    alignments = [coord for coord in file_reader if not coord.is_self_hit()]
+    return alignments
+
+
+@click.command()
+@click.option("--fref", '-r', help="Host reference genome")
+@click.option("--fprophage", '-p', help="Predicted prophage FASTA")
+@click.option("--fout", '-o', help="output file name")
+def main(fref, fprophage, fout):
+    refs = list(SeqIO.parse(fref, "genbank"))
+    prophages = list(SeqIO.parse(fprophage, "fasta"))
+    
+    alns = []
+    for qry in prophages:
+        for ref in refs:
+            aln = run_nucmer(qry, ref)
+            if len(aln) > 0:
+                ref_start = min([x.ref_start for x in aln])
+                ref_end = max([x.ref_end for x in aln])
+                alns.append([ref.id, ref_start, ref_end, qry.id])
+    with open(fout, "w") as fh:
+        fh.writelines(['\t'.join(x) for x in alns])
+
+
+if __name__ == '__main__':
+    main()
